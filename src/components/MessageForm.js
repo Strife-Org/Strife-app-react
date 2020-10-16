e0fcc50771da01e271485995f62097bcfbebf7f1
Nemesisly <34484174+Nemesisly@users.noreply.github.com>
2020-10-16 12:45:17 -0300

    commit

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -40,8 +40,8 @@ function MessageForm(props) {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.contentEditableContainer} onKeyPressCapture={handleKeyPress}>
+    <div>
+      <div onKeyPressCapture={handleKeyPress}>
         <div
           id="message"
           contentEditable="true"
